da4b05994e26693b0ac42890ac6e69d8dacd28a7
Rohith Nair <rohithnair2910@gmail.com>
2025-04-02 17:12:54 +0530

    Sider Images Responsive Updates

--- components/Home/home.js
+++ components/Home/home.js
@@ -21,29 +21,31 @@ const Home = function (props) {
   console.log("isPending: ",isPending);
 
   return (
-      <div>
-        <div className='container-fluid'>
-          <div className='row no-gutters'>
-            <div className='col-sm-4 col-md-5 border border-4 border-bottom-0 border-start-0 border-end-0 p-0 m-0'/>
-            <div className='col-md-2'>
-              <div className='fs-2 font-weight-bolder text-center'>
-                WELCOME
-              </div>
+    <div>
+      <div className='container-fluid'>
+        <div className='row no-gutters'>
+          <div className='col-sm-4 col-md-5 border border-4 border-bottom-0 border-start-0 border-end-0 p-0 m-0'/>
+          <div className='col-md-2'>
+            <div className='fs-2 font-weight-bolder text-center'>
+              WELCOME
             </div>
-            <div className='col-md-5 border border-4 border-top-0 border-start-0 border-end-0 p-0 m-0'/>
           </div>
+          <div className='col-md-5 border border-4 border-top-0 border-start-0 border-end-0 p-0 m-0'/>
         </div>
-        <div className='container-fluid pt-4'>
-          <div className='row'>
-            <div className='col-sm-12 col-md-9'>
-              <Slider images={ImagesList} />
-            </div>
-            <div className='col-sm-12 col-md-3'>
+      </div>
+      <div className='container-fluid pt-5'>
+        <div className='row'>
+          <div className='col-sm-12 col-xl-9 order-xl-0 order-1'>
+            <Slider images={ImagesList} />
+          </div>
+          <div className='col-sm-12 col-xl-3 pt-4 pt-xl-0 pb-4 pb-xl-0 order-xl-1 order-0'>
+            <div className='row'>
               <Image src={SnowMountain} height={'550'} width={'auto'} alt={`right-image`} />
             </div>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
