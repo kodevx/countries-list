f8461a7d70f135e0fc610ebd54bce8d2eb50f935
Rohith Nair <rohithnair2910@gmail.com>
2025-04-04 09:36:54 +0530

    Region Links Style Updates in Header Section

--- components/Header/header.js
+++ components/Header/header.js
@@ -28,37 +28,37 @@ const Header = (props) => {
                     <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end align-center'>
                     <ul className="navbar-nav mb-2 mx-lg-0">
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'All' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('All')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'All' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('All')}>
                                     All
                                 </button>
                             </li>
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Asia' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Asia')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Asia' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Asia')}>
                                     Asia
                                 </button>
                             </li>
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Africa' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Africa')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Africa' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Africa')}>
                                     Africa
                                 </button>
                             </li>
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Europe' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Europe')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Europe' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Europe')}>
                                     Europe
                                 </button>
                             </li>
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Americas' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Americas')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Americas' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Americas')}>
                                     Americas
                                 </button>
                             </li>
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Oceania' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Oceania')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Oceania' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Oceania')}>
                                     Oceania
                                 </button>
                             </li>
                             <li className="nav-item mx-2">
-                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Polar' ? 'border-4 border-dark': 'border-bottom-0 border-light'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Polar')}>
+                                <button className={`nav-link ps-0 pe-3 py-2 border border-top-0 border-start-0 border-end-0 ${selectedRegion === 'Polar' ? 'border-4 border-dark fw-semibold': 'border-bottom-0 border-light text-body-secondary'} ${isLinksActive ? 'active border border-bottom-3' : 'disabled'}`} onClick={() => handleRegionAndCountries('Polar')}>
                                     Polar
                                 </button>
                             </li>
