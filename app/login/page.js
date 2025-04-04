38c8b02b2fe8f897a81edd11c98564f952260cef
Rohith Nair <rohithnair2910@gmail.com>
2025-04-04 18:03:34 +0530

    Added Login Page

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
+import LoginImage from '@/assets/images/loginImage.webp';
+
+import SignIn from '@/components/SignIn';
 
 const Login = (props) => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-12 col-lg-7'>
+          <SignIn />
+        </div>
+        <div className='col-12 col-lg-5'>
+          <Image src={LoginImage} height={'450'} width={'auto'} alt={'login-image'} />
+        </div>
+      </div>
     </div>
   );
 }
